c489c727e907d0130887c934b9c1b279d9d5330d
Charukesh7884 <Charukesh7884@gmail.com>
2025-04-25 11:07:16 +0200

    Success

--- msqueue.c
+++ msqueue.c
@@ -1,28 +1,3 @@
-# Linux-IPC-Message-Queues
-Linux IPC-Message Queues
-
-# AIM:
-To write a C program that receives a message from message queue and display them
-
-# DESIGN STEPS:
-
-### Step 1:
-
-Navigate to any Linux environment installed on the system or installed inside a virtual environment like virtual box/vmware or online linux JSLinux (https://bellard.org/jslinux/vm.html?url=alpine-x86.cfg&mem=192) or docker.
-
-### Step 2:
-
-Write the C Program using Linux message queues API 
-
-### Step 3:
-
-Execute the C Program for the desired output. 
-
-# PROGRAM:
-
-## C program that receives a message from message queue and display them
-
-```
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -42,6 +17,7 @@ int main(int argc, char *argv[]) {
         printf("Usage: %s writer|reader\n", argv[0]);
         return 1;
     }
+
  key = ftok("msqueue.c", 65);
     if (key == -1) {
         perror("ftok");
@@ -79,14 +55,3 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-```
-
-## OUTPUT
-
-![writer](IMG/writer.png )
-![reader](IMG/reader.png )
-![ipcs](IMG/ipcs.png)
-
-
-# RESULT:
-The programs are executed successfully.
